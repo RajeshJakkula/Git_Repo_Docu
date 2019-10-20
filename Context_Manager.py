76f589aea89e5a5eef8e3c981aadeb9a8d4bdcf4
Rajesh Jakkula <rajeshjakkula@Rajeshs-MBP.home>
2019-10-19 19:23:48 -0700

    Changes

--- Context_Manager.py
+++ Context_Manager.py
@@ -3,7 +3,7 @@ class File():
     def __init__(self, filename, mode):
         self.filename = filename
         self.mode = mode
-
+    # Enter method to open the connection
     def __enter__(self):
         self.open_file = open(self.filename, self.mode)
         return self.open_file
