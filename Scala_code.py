bc1e29e5bf1abb899444585a8ff88f27430950a1
Rajesh Jakkula <rajeshjakkula@Rajeshs-MBP.home>
2019-10-21 15:39:45 -0700

    Scala Code first to ScalaRepo

--- Scala_code.py
+++ Scala_code.py
@@ -7,3 +7,12 @@ def add_numbers(numbers):
 
 input=add_numbers([1,2,3,4,5,6])
 print(next(input))
+
+# changes are here
+
+def add_numbers(numbers):
+    for i in numbers:
+        print(i+i)
+
+input=add_numbers([1,2,3,4,5,6])
+print(next(input))
